2388dbde6c4a06d36b8a9d626b869ad59368c608
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2025-07-20 00:57:09 +0200

    Move easter egg player file to utils

--- lib/screens_pages/settings_page.dart
+++ lib/screens_pages/settings_page.dart
@@ -9,7 +9,7 @@ import 'package:simple_chat/utils/colorimetry_blueprints.dart';
 
 import 'package:simple_chat/utils/logger_config.dart';
 
-import '../message_related/easter_egg_player.dart';
+import '../utils/easter_egg_player.dart';
 
 class settings extends StatefulWidget {
   const settings({super.key});
